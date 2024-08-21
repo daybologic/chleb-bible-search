75756d5a4fc50e07c5554d4b2e07cb0da3b16803
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2024-08-21 18:11:22 +0100

    🗃️ Remove DynamoDB support
    This was not used and was just an idea

--- bin/import/verse.sh
+++ bin/import/verse.sh
@@ -42,37 +42,9 @@ TRANSLATION='kjv'
 ref="${TRANSLATION}:${bookNameShort}:${chapter}:${verse}"
 outputInterim="data/${TRANSLATION}-verses.txt"
 
-if false; then
-	text=$(echo -n "$text" | jq -Rsa .)
-	aws --profile palmer dynamodb put-item --table-name bible --item \
-	   "{
-	       \"ref\": {
-		   \"S\": \"$ref\"
-	       },
-	       \"translation\": {
-		   \"S\": \"$TRANSLATION\"
-	       },
-	       \"bookNameShort\": {
-		   \"S\": \"$bookNameShort\"
-	       },
-	       \"bookNameLong\": {
-		   \"S\": \"$bookNameLong\"
-	       },
-	       \"chapter\": {
-		   \"S\": \"$chapter\"
-	       },
-	       \"verse\": {
-		   \"S\": \"$verse\"
-	       },
-	       \"text\": {
-		   \"S\": $text
-	       }
-	   }"
-else
-	verseFile=$(mktemp bible-master-book.XXXXXX.tmp --tmpdir)
-	echo "${ref}::${text}" | tee "$verseFile"
-	cat "$verseFile" >> "$outputInterim"
-	rm -vf "$verseFile"
-fi
+verseFile=$(mktemp bible-master-book.XXXXXX.tmp --tmpdir)
+echo "${ref}::${text}" | tee "$verseFile"
+cat "$verseFile" >> "$outputInterim"
+rm -vf "$verseFile"
 
 exit 0
