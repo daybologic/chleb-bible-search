eb97c915c17a9e23c331eeea3d8af142a4fe57ae
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2024-09-17 21:31:58 +0100

    🐛 workaround case-correction for short book names starting with digits
    The following error was seen when traversing books via next links:
    [WARN ] (Religion::Bible::Verses L76): Short book name '1sam' is not a book in the bible

--- lib/Religion/Bible/Verses.pm
+++ lib/Religion/Bible/Verses.pm
@@ -65,7 +65,11 @@ sub getBookByShortName {
 	my ($self, $shortName, $unfatal) = @_;
 
 	$shortName ||= '';
-	$shortName = "\u$shortName";
+	if ($shortName =~ m/^(\d)(\w+)$/) {
+		$shortName = "$1\u$2";
+	} else {
+		$shortName = "\u$shortName";
+	}
 
 	foreach my $book (@{ $self->books }) {
 		next if ($book->shortName ne $shortName);
