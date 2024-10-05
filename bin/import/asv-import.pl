450052dc875cfab169db4e774ff062f2cce60265
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2024-10-05 13:11:31 +0100

    ⚗️ import ASV script (EXP)

---
+++ bin/import/asv-import.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/env perl
+
+package main;
+use strict;
+use warnings;
+
+use English;
+use IO::File;
+
+sub processBook {
+	my ($bible, $bookShortName, $bookLongName) = @_;
+	my $fileName = sprintf('%s.txt', $bookShortName);
+	$fileName = sprintf('%s.txt', $bookLongName) unless (-f $fileName);
+	if (my $fh = IO::File->new($fileName, 'r')) {
+		my $bigLine = '';
+		while (my $line = <$fh>) {
+			$line =~ s/\n/ /;
+			$bigLine .= $line;
+		}
+		my (@rawVerses) = split(m/\{(\d+):(\d+)\}/, $bigLine);
+		my ($chapter, $verse) = (0, 0);
+		my $verseCount = 0;
+		for (my $i = 0; $i < scalar(@rawVerses); $i++) {
+			if ($i % 3 == 0) { # text
+				my $text = $rawVerses[$i];
+				$text =~ s/^\s+//;
+				$text =~ s/\s+$//;
+				if ($chapter > 0) {
+					#printf("%s\n\n", $text);
+					$verseCount++;
+					$bible->{$bookShortName}->{$chapter}->{$verse} = $text;
+				}
+			} elsif ($i % 2 == 0) {
+				$chapter = $rawVerses[$i];
+				#printf("verse %d\n", $verse);
+			} else {
+				$verse = $rawVerses[$i];
+				#printf("chapter %d\n", $chapter);
+			}
+		}
+		$fh->close();
+		printf("total count %d for %s\n", $verseCount, $fileName);
+	} else {
+		printf(STDERR "'%s' -- %s\n", $fileName, $ERRNO);
+		die('Must process all books!');
+	}
+
+	return;
+}
+
+sub writeBible {
+	my ($bible) = @_;
+
+	if (my $fh = IO::File->new('asv.txt', 'w')) {
+		foreach my $book (sort(keys(%$bible))) {
+			foreach my $chapter (sort(keys(%{ $bible->{ $book } }))) {
+				foreach my $verse (sort(keys(%{ $bible->{ $book }->{ $chapter } }))) {
+					printf($fh "asv:%s:%d:%d:%s\n", $book, $chapter, $verse, $bible->{ $book }->{ $chapter }->{ $verse });
+				}
+			}
+		}
+	}
+
+	return;
+}
+
+sub main {
+	my %bible = ( );
+	if (my $fh = IO::File->new('kjv.cvs', 'r')) { # yes this is a misnomer, and I am using the kjv index to process asv
+		while (my $line = <$fh>) {
+			chomp($line);
+			my ($bookShortName, $numChapters, $bookLongName) = split(m/;/, $line);
+			printf("%s -> %d\n", $bookShortName, $numChapters);
+			processBook(\%bible, $bookShortName, $bookLongName);
+		}
+		$fh->close();
+		warn('FIXME: There are text files unprocessed.  What are they and why?  Philemon, for example?');
+	} else {
+		printf(STDERR "ERROR: $ERRNO");
+		return 1;
+	}
+
+	writeBible(\%bible);
+
+	return 0;
+}
+
+exit(main()) unless (caller());
