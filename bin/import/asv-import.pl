a0b8ea057dfe2cddb1f13d79c9a8cece6aa48cc1
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2024-10-06 13:02:55 +0100

    📚 ASV bible correctly generated, spot checks all work

--- bin/import/asv-import.pl
+++ bin/import/asv-import.pl
@@ -11,7 +11,7 @@ use IO::File;
 my $globalVerseCount = 0;
 
 sub processBook {
-	my ($bible, $bookShortName) = @_;
+	my ($bible, $bible2, $bookShortName) = @_;
 	my $fileName = sprintf('%s.txt', $bookShortName);
 	if (my $fh = IO::File->new($fileName, 'r')) {
 		my $bigLine = '';
@@ -21,29 +21,51 @@ sub processBook {
 		}
 		my (@rawVerses) = split(m/\{(\d+):(\d+)\}/, $bigLine);
 		warn scalar(@rawVerses) / 3 if ($bookShortName eq 'Rev');
-		print Dumper \@rawVerses;
+		#print Dumper \@rawVerses;
+		#die 'trap' if ($bookShortName eq 'Rev');
 		my ($chapter, $verse) = (0, 0);
 		my $verseCount = 0;
-		for (my $i = 0; $i < scalar(@rawVerses); $i++) {
-			if ($i % 3 == 0) { # text
+		my $mode = 0;
+		for (my $i = 1; $i < scalar(@rawVerses); $i++) {
+			if ($mode == 2) { # text
 				my $text = $rawVerses[$i];
 				$text =~ s/^\s+//;
 				$text =~ s/\s+$//;
 				if ($chapter > 0) {
 					printf("asv:%s:%d:%d::%s\n", $bookShortName, $chapter, $verse, $text) if ($bookShortName eq 'Rev');
+
+					my $key2 = sprintf("asv:%s:%d:%d", $bookShortName, $chapter, $verse);
+					printf("key2: $key2\n") if ($key2 =~ m/^asv:/);
 					if ($bible->{$bookShortName}->{$chapter}->{$verse}) {
-						die(sprintf("'asv:%s:%d:%d' already exists", $bookShortName, $chapter, $verse));
+						if (0) {
+							warn(sprintf("'asv:%s:%d:%d' already exists: %s", $bookShortName, $chapter, $verse,
+							    $bible->{$bookShortName}->{$chapter}->{$verse}));
+						}
+					} else {
+						$bible->{$bookShortName}->{$chapter}->{$verse} = $text;
 					}
-					$bible->{$bookShortName}->{$chapter}->{$verse} = $text;
+
+					if ($bible2->{$key2}) {
+						if ($bookShortName eq 'Rev') {
+							die(sprintf("'%s' already exists:\n%s\n%s\n", $key2, $bible2->{$key2}, $text))
+						}
+					} else {
+						$bible2->{$key2} = $text;
+					}
+
 					$verseCount++;
 				}
-			} elsif ($i % 2 == 0) {
+				$mode = 0;
+			} elsif ($mode == 0) {
 				$chapter = $rawVerses[$i];
-				#printf("chapter %d\n", $chapter);
-			} else {
+				printf("chapter %d\n", $chapter);
+				$mode = 1;
+			} elsif ($mode == 1) {
 				$verse = $rawVerses[$i];
-				#printf("verse %d\n", $verse);
+				printf("verse %d\n", $verse);
+				$mode = 2;
 			}
+#			die $i if ($i == 7);
 		}
 		$fh->close();
 		printf("verse count %d for %s\n", $verseCount, $fileName) if ($bookShortName eq 'Rev');
@@ -89,14 +111,39 @@ sub writeBible {
 	return;
 }
 
+sub writeBible2 {
+	my ($bible2) = @_;
+
+	my $writtenVerseCount = 0;
+	my $writtenBookCount = 0;
+	my $revelationVerseCount = 0;
+	my $fileName = 'asv.txt';
+	if (my $fh = IO::File->new($fileName, 'w')) {
+		foreach my $key2 (sort(keys(%$bible2))) {
+			printf($fh "%s::%s\n", $key2, $bible2->{$key2});
+			$writtenVerseCount++;
+		}
+	}
+
+	my $msg = sprintf("Bible2: Wrote %d verses to '%s'", $writtenVerseCount, $fileName);
+	if ($writtenVerseCount == 31_102) {
+		printf("%s\n", $msg);
+	} else {
+		die("ERROR! $msg");
+	}
+
+	return;
+}
+
 sub main {
 	my %bible = ( );
+	my %bible2 = ( );
 	if (my $fh = IO::File->new('kjv.cvs', 'r')) { # yes this is a misnomer, and I am using the kjv index to process asv
 		while (my $line = <$fh>) {
 			chomp($line);
 			my ($bookShortName, $numChapters) = split(m/;/, $line);
 			printf("%s -> %d\n", $bookShortName, $numChapters) if ($bookShortName eq 'Rev');
-			processBook(\%bible, $bookShortName);
+			processBook(\%bible, \%bible2, $bookShortName);
 		}
 		$fh->close();
 	} else {
@@ -111,6 +158,7 @@ sub main {
 	die("Verse count wrong!  Must be 31,102: $globalVerseCount") if ($globalVerseCount != 31_102);
 
 	writeBible(\%bible);
+	writeBible2(\%bible2);
 
 	return 0;
 }
