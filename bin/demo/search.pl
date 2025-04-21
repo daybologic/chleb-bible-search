6e9772ee9b920835a71374728bfcf59454eb1746
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2025-04-21 17:11:30 +0100

    🩹 pass translation to bible object

--- bin/demo/search.pl
+++ bin/demo/search.pl
@@ -10,7 +10,7 @@ use Chleb::Bible;
 
 sub main {
 	my ($query) = (@ARGV);
-	my $bible = Chleb::Bible->new();
+	my $bible = Chleb::Bible->new({ translation => 'kjv' });
 
 	$query = $bible->newSearchQuery($query)->setLimit(2);
 	# FIXME: Need to limit to one book?  should be able to do this via Query.pm
