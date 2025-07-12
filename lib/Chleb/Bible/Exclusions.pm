65eddfc82959ac0e4289f8521951db44b079edeb
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2025-07-12 12:31:45 +0100

    ♻️ Simplify loop
    https://github.com/daybologic/chleb-bible-search/pull/78/files#r2202540043

--- lib/Chleb/Bible/Exclusions.pm
+++ lib/Chleb/Bible/Exclusions.pm
@@ -55,14 +55,11 @@ sub __makeTerms {
 
 	my @terms;
 	my $i = 0;
-	my $term;
 	my $terms = $self->dic->config->get($SECTION_NAME, 'terms', [ ]);
 
-	do {
-		if ($term = $terms->[$i++]) {
-			push(@terms, $term);
-		}
-	} while ($term);
+	while (my $term = $terms->[$i++]) {
+		push(@terms, $term);
+	}
 
 	$self->dic->logger->debug(sprintf('Loaded %d excluded VoTD terms', scalar(@terms)));
 	return \@terms;
@@ -73,39 +70,36 @@ sub __makeRefs {
 
 	my @refs;
 	my $i = 0;
-	my $ref;
 	my $key = 'refs';
 	my $refs = $self->dic->config->get($SECTION_NAME, $key, [ ]);
 
-	do {
-		if ($ref = $refs->[$i++]) {
-			my ($bookName, $chapterOrdinal, $verseOrdinalStart, $verseOrdinalEnd);
-			if ($ref =~ m/^(\w+)\s+(\d+):(\d+)-(\d+)$/) {
-				($bookName, $chapterOrdinal, $verseOrdinalStart, $verseOrdinalEnd) = ($1, $2, $3, $4);
-			} elsif ($ref =~ m/^(\w+)\s+(\d+):(\d+)$/) {
-				($bookName, $chapterOrdinal, $verseOrdinalStart) = ($1, $2, $3);
-			} else {
-				$self->dic->logger->error(sprintf('%s has been ignored because the format was not recognized', $key));
-			}
+	while (my $ref = $refs->[$i++]) {
+		my ($bookName, $chapterOrdinal, $verseOrdinalStart, $verseOrdinalEnd);
+		if ($ref =~ m/^(\w+)\s+(\d+):(\d+)-(\d+)$/) {
+			($bookName, $chapterOrdinal, $verseOrdinalStart, $verseOrdinalEnd) = ($1, $2, $3, $4);
+		} elsif ($ref =~ m/^(\w+)\s+(\d+):(\d+)$/) {
+			($bookName, $chapterOrdinal, $verseOrdinalStart) = ($1, $2, $3);
+		} else {
+			$self->dic->logger->error(sprintf('%s has been ignored because the format was not recognized', $key));
+		}
 
-			if ($bookName) {
-				my $verse;
-				$verseOrdinalEnd = $verseOrdinalStart if (!$verseOrdinalEnd || $verseOrdinalEnd < $verseOrdinalStart);
-				for (my $verseOrdinal = $verseOrdinalStart; $verseOrdinal <= $verseOrdinalEnd; $verseOrdinal++) {
-					eval {
-						# FIXME: Deprecated!  This is highly unreliable and you need to find a new
-						# way to access the library afore it goes away!
-						$verse = $self->dic->bible->fetch($bookName, $chapterOrdinal, $verseOrdinal);
-					};
-					if (my $evalError = $EVAL_ERROR) {
-						$self->dic->logger->error(sprintf('%s load failed: %s', $key, $evalError));
-					} else {
-						push(@refs, $verse);
-					}
+		if ($bookName) {
+			my $verse;
+			$verseOrdinalEnd = $verseOrdinalStart if (!$verseOrdinalEnd || $verseOrdinalEnd < $verseOrdinalStart);
+			for (my $verseOrdinal = $verseOrdinalStart; $verseOrdinal <= $verseOrdinalEnd; $verseOrdinal++) {
+				eval {
+					# FIXME: Deprecated!  This is highly unreliable and you need to find a new
+					# way to access the library afore it goes away!
+					$verse = $self->dic->bible->fetch($bookName, $chapterOrdinal, $verseOrdinal);
+				};
+				if (my $evalError = $EVAL_ERROR) {
+					$self->dic->logger->error(sprintf('%s load failed: %s', $key, $evalError));
+				} else {
+					push(@refs, $verse);
 				}
 			}
 		}
-	} while ($ref);
+	}
 
 	$self->dic->logger->debug(sprintf('Loaded %d excluded VoTD references', scalar(@refs)));
 	return \@refs;
