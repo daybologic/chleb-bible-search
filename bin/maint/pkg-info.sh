0b0eddc48813f70c450365422001df8ddc24988c
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2025-07-14 21:10:17 +0100

    🧐 turn on script debug mode

--- bin/maint/pkg-info.sh
+++ bin/maint/pkg-info.sh
@@ -29,6 +29,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+set -x
+
 outFile='lib/Chleb/Generated/Info.pm'
 
 buildUser=$(whoami)
