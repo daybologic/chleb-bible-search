9ec148a72195220ab95385c50947723af78047dd
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2025-07-14 22:24:04 +0100

    ⏪ backout 0b0eddc

--- bin/maint/pkg-info.sh
+++ bin/maint/pkg-info.sh
@@ -29,8 +29,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-set -x
-
 outFile='lib/Chleb/Generated/Info.pm'
 
 buildUser=$(whoami)
