6c9bbba2ed6607e27b7c62b0f0beaa9b8c89a42d
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2026-03-03 07:56:37 +0000

    🐇 implement some of the hook install robustness CodeRabbit suggestions
    https://github.com/daybologic/chleb-bible-search/pull/171#issuecomment-3978678042
    
    I haven't implemented everything and I've done of it in a slightly
    different way.  For example, we set the copy executable, not the
    original repo copy.

--- bin/maint/git-install-local-hooks.sh
+++ bin/maint/git-install-local-hooks.sh
@@ -36,13 +36,26 @@
 # turn on errors and unbound variable trap
 set -eu
 
-if [ -d .git ]; then
+if [ -d '.git' ]; then
+	if [ ! -d '.git/hooks/' ]; then
+		>&2 echo 'ERROR: Directory .git/hooks/ does not exist'
+		exit 1
+	fi
+
 	pre-commit install --install-hooks
 	for hook in bin/git/hooks/post-*; do
-		cp -v "$hook" .git/hooks/
+		hookTarget=$(basename $hook)
+		hookTarget=".git/hooks/$hookTarget"
+		if ! cmp -s "$hook" "$hookTarget"; then
+			cp -v "$hook" "$hookTarget"
+		fi
+
+		if [ ! -x "$hookTarget" ]; then
+			chmod +x "$hookTarget"
+		fi
 	done
 else
-	echo "WARN: .git not found" >&2
+	>&2 echo "WARN: .git not found" >&2
 fi
 
 exit 0
